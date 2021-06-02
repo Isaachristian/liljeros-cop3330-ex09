2b1fcb3788e51ca1755daa94e1389f436917273c
Isaac Liljeros <isaak.liljeros@gmail.com>
2021-06-01 20:04:28 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    private static double areaPerCan = 350;
+
+    public static void main( String[] args ) {
+        Scanner input = new Scanner(System.in);
+
+        String output = " cans of paint";
+
+        System.out.print("Input the area to cover (in 'sq ft'): ");
+        int area = input.nextInt();
+
+        int cansNeeded = paintCalc((double)area);
+        if (cansNeeded == 1)
+            output = " can of paint";
+
+        System.out.println("you will need " + cansNeeded + output);
+    }
+
+    private static int paintCalc(double area) {
+        double cans = Math.ceil(area / areaPerCan);
+        return (int)cans;
+    }
+}
